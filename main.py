db1e1da43c6606a72ebad27fa9feb6f3516b041c
maestroTW <nayan10122007@gmail.com>
2025-03-20 20:03:09 +0500

    release main
    core function based on Canny Edge Detection and PIL filters

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from PIL import Image, ImageFilter, ImageEnhance, ImageOps
 import numpy as np
 import cv2
 
-
 def make_coloring_page(image_path):
     # open
     img = Image.open(image_path)
 
+    # improve quality
+    contrast_enhancer = ImageEnhance.Contrast(img)
+    high_contrast_img = contrast_enhancer.enhance(1.5)
+    brightness_enhancer = ImageEnhance.Brightness(high_contrast_img)
+    brighter_img = brightness_enhancer.enhance(1.2)
+
     # Canny Edge Detection
-    array = np.array(img)
+    array = np.array(brighter_img)
     canny_edges = cv2.Canny(array, 100, 200)
     canny_edges = Image.fromarray(canny_edges)
 
@@ -19,6 +24,6 @@ def make_coloring_page(image_path):
     return inverted_edges
 
 
-image_path = 'test.jpg'
+image_path = 'examples/test.jpg'
 coloring_page = make_coloring_page(image_path)
 coloring_page.show()
